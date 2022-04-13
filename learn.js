c05fe6e8846794f971258a72df255204d625accc
ivoteT <103273470+ivoteT@users.noreply.github.com>
2022-04-13 10:08:15 -0500

    afsdfsdfff

--- learn.js
+++ learn.js
@@ -1,5 +1,5 @@
 function code() {
-    alert("why did the chicken cross the road to get to the idoits house Knock knock whos there the idoit");
+    alert("why did the chicken cross the road to get to the idoits house Knock knock whos there the the chickan");
     }
     
     
